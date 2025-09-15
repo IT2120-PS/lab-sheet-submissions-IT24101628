b10dc2e47a6a4468ec4b9fcf403b517fa18bdc67
IT24101628 <IT24101628@my.sliit.lk>
2025-09-15 19:11:18 +0530

    Lab 07

---
+++ Lab Sheet 07/IT24101628_Lab07.R
@@ -0,0 +1,9 @@
+#Q1
+punif(25,min =0, max =40) - punif(10,min =0, max =40)
+ 
+#Q2
+pexp(2, rate=0.33,lower.tail=TRUE)
+
+#Q3
+pnorm(130, mean =100, sd =15,lower.tail =FALSE)
+qnorm(0.95,mean =100, sd =15)
